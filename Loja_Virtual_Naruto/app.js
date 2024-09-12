3c340d4eca7be3b5c8432fa441b6ffa8665f84bd
barbarachacon <barbara.chaves@fatec.sp.gov.br>
2024-09-12 01:13:44 -0300

    Add files via upload

--- Loja_Virtual_Naruto/app.js
+++ Loja_Virtual_Naruto/app.js
@@ -4,15 +4,15 @@ function dados() {
                {id:1, login:"Bruna", password:"1234@", email:"bruna@gmail.com"},
                {id:2, login:"Dilan", password:"1234@", email:"dilan@gmail.com"},
                {id:3, login:"Vinicius", password:"1234@", email:"Vinicius@gmail.com"}
-              ]
+              ];
               
-    return ds
+    return ds;
 
 }
 
-console.log(dados()[1].login)
+const usuarios = dados();
 
-const usuarios = dados()
+console.log(dados()[1].login);
 
 // console.log("O seu Login é: " + usuarios[0].login + "\n A sua senha é: " + usuarios[0].password)
 
@@ -21,11 +21,11 @@ const usuarios = dados()
 
 function login() {
 
-    let log = document.querySelector(login).value
-    let senha = document.querySelector(password).value
+    let log = document.querySelector('#login').value;
+    let senha = document.querySelector('#password').value;
 
-    if (log == usuarios[1].login && senha == usuarios[1].password) {
-        console.log("Você logou!")
+    if (log == usuarios[1].login && senha === usuarios[1].password) {
+        console.log("Você logou!");
 }
 }
 
